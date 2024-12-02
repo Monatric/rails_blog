c7291b5ced1f5d30cd5cf2c07694c301d542fefc
Monatric <seandumlao03@gmail.com>
2024-12-02 12:56:51 +0800

    Fix: revert environments changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "railsblog-production-b424.up.railway.app"
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  # config.hosts << "railsblog-production-b424.up.railway.app"
+  # config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
